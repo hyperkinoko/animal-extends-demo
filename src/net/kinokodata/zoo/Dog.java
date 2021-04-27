1e159d6c445b84ac4a57387d5ebfd5c30396ef6b
hyperkinoko <superkinoko@icloud.com>
2021-04-27 18:01:03 +0900

    継承を説明するためのコード

--- src/net/kinokodata/zoo/Dog.java
+++ src/net/kinokodata/zoo/Dog.java
@@ -1,6 +1,10 @@
 package net.kinokodata.zoo;
 
 public class Dog extends Animal {
+    // ポリモーフィズムの考え方
+    // 猫も犬も喋るが、それぞれ別の喋り方をする
+    // thisは自分自身
+    // nameは親から継承したインスタンス変数
     @Override
     public void speak() {
         System.out.println("わんわん。ぼくは" + this.name + "だわん！");
